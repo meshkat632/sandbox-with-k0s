385088f39322480818cfae5846895976cd88edbf
Meshkatul Anwer <meshkat632@gmail.com>
2026-09-18 12:32:20 +0200

    refactor(infra): remove inline SOPS secret code

--- infra/main.tf
+++ infra/main.tf
@@ -10,33 +10,6 @@ provider "aws" {
   }
 }
 
-
-/*
-
-i need to make the follow as a callable module from tf-modules folder
-
-*/
-
-# secrets.tf
-data "sops_file" "tokens" {
-  source_file = "${path.module}/secrets/tokens.secrets.yaml"
-}
-
-output "tokens_debug" {
-  value     = data.sops_file.tokens.raw
-  sensitive = true
-}
-
-resource "aws_secretsmanager_secret" "tokens" {
-  name = "sandbox/tokens-test-v1"
-}
-
-resource "aws_secretsmanager_secret_version" "tokens" {
-  secret_id     = aws_secretsmanager_secret.tokens.id
-  secret_string = data.sops_file.tokens.raw
-}
-
-
 module "tokens_secret" {
   source      = "./tf-modules/secrets-from-sops"
   source_file = "${path.root}/secrets/tokens.secrets.yaml"
